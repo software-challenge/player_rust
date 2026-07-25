244df3e33d9a3a73648a3a493802debfea1f2ce4
SturmEnte <jonas@sturmente.de>
2026-07-26 00:09:05 +0200

    chore

--- tests/integration_test.rs
+++
@@ -1,7 +0,0 @@
-use socha::connection::handler::ConnectionHandler;
-
-#[test]
-fn connection_test() {
-    let mut connection = ConnectionHandler::new().unwrap();
-    connection.join(None).unwrap();
-}
