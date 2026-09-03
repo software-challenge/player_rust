f3a055b0fc8fb5b23ddaad64e02deb1eced1b6a2
Jonas <jonas@sturmente.de>
2026-09-03 16:05:25 +0200

    refactor: Remove debug print

--- src/game/board.rs
+++ src/game/board.rs
@@ -38,8 +38,6 @@ impl Board {
             let new_x = x + coord.x as usize;
             let new_y = y + coord.y as usize;
 
-            println!("Placing piece block at: {} {}", new_x, new_y);
-
             self.board[new_y][new_x] = Some(team);
         }
     }
