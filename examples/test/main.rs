031495ac1caf8c46363e9970ac71b4f51035f4a9
Jonas <jonas@sturmente.de>
2026-09-11 20:36:38 +0200

    chore: Remove testing code

--- examples/test/main.rs
+++ examples/test/main.rs
@@ -1,4 +1,4 @@
-use socha::{game::team::Team, prelude::*};
+use socha::{prelude::*};
 
 struct Player {
     game_state: Option<GameState>,
@@ -17,14 +17,6 @@ impl Client for Player {
     fn on_game_state_updated(&mut self, game_state: GameState ) {
         game_state.get_board().print_board();
         self.game_state = Some(game_state);
-        
-        // Print points of both teams
-        let team_one_points = self.game_state.as_ref().unwrap().get_points_for_team(&Team::One);
-        let team_two_points = self.game_state.as_ref().unwrap().get_points_for_team(&Team::Two);
-        println!("Team One Points: {}", team_one_points);
-        println!("Team Two Points: {}", team_two_points);
-        
-        println!("Game state updated!");
     }
 }
 
