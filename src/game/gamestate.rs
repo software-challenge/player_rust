f026ad04838023d33b917931a8cf89ff11980999
Jonas <jonas@sturmente.de>
2026-09-11 19:55:48 +0200

    refactor: Moved duplicated code into function

--- src/game/gamestate.rs
+++ src/game/gamestate.rs
@@ -39,50 +39,23 @@ impl GameState {
     /// If the move is invalid, this function may lead to an inconsistent game state.
     pub fn apply_move_unchecked(&mut self, m: &Move, turn: u8) {
         self.board.place_piece_unchecked(m.x, m.y, m.color, Piece::new(m.piece, m.rotation, m.is_flipped));
-
-        // Remove used piece from the corresponding color's available pieces
-        match m.color {
-            Color::Blue => {
-                self.pieces[0].retain(|&p| p != m.piece);
-            },
-            Color::Yellow => {
-                self.pieces[1].retain(|&p| p != m.piece);
-            },
-            Color::Red => {
-                self.pieces[2].retain(|&p| p != m.piece);
-            },
-            Color::Green => {
-                self.pieces[3].retain(|&p| p != m.piece);
-            },
-        }
-
-        self.turn = turn;
-
-        if self.turn.is_multiple_of(4) {
-           self.round += 1;
-        }
-
-        const COLOR_ORDER_ONE: [Color; 4] = [Color::Blue, Color::Yellow, Color::Red, Color::Green];
-        const COLOR_ORDER_TWO: [Color; 4] = [Color::Yellow, Color::Red, Color::Green, Color::Blue];
-
-        // Current color can be caluclated from turn number
-        if self.is_starting_team_one {
-            self.current_turn_color = COLOR_ORDER_ONE[(self.turn % 4) as usize];
-        } else {
-           self.current_turn_color = COLOR_ORDER_TWO[(self.turn % 4) as usize];
-        }
+        self.update_gamestate(m, turn);
     }
 
     /// Applies a move to the game state with validation.
     /// Returns true if the move was valid and applied, false otherwise.
     pub fn apply_move(&mut self, m: &Move, turn: u8) -> bool {
-
         if gamerulelogic::is_valid_move(&self, m) == false {
             return false;
         }
 
         self.board.place_piece(m.x, m.y, m.color, Piece::new(m.piece, m.rotation, m.is_flipped));
+        self.update_gamestate(m, turn);
 
+        true
+    }
+
+    fn update_gamestate(&mut self, m: &Move, turn: u8) {
         // Remove used piece from the corresponding color's available pieces
         match m.color {
             Color::Blue => {
@@ -114,8 +87,6 @@ impl GameState {
         } else {
            self.current_turn_color = COLOR_ORDER_TWO[(self.turn % 4) as usize];
         }
-
-        true
     }
 
     pub fn get_current_turn_color(&self) -> &Color {
